52795ecda3484bb54573964dd279fad3b835a315
Genna <Genna@ENGINE>
2022-01-30 13:13:53 +0300

    Add project files.

---
+++ Data/TestDbKarasevContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestDbKarasev.Models;
+
+namespace TestDbKarasev.Data
+{
+    public class TestDbKarasevContext : DbContext
+    {
+        public TestDbKarasevContext (DbContextOptions<TestDbKarasevContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestDbKarasev.Models.Product> Product { get; set; }
+    }
+}
---
+++ Models/Class.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestDbKarasev.Models
+{
+    public class ProductsContext : DbContext
+    {
+        public DbSet<Product> Product { get; set; }
+        public ProductsContext (DbContextOptions <ProductsContext> options)
+            : base (options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
